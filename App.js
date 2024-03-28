c6260418f854656fdf9673a9948728723fddeecc
Matteo Didone <matteo.dido9@gmail.com>
2024-03-28 17:50:42 +0100

    Changed backgroundColor

--- App.js
+++ App.js
@@ -13,7 +13,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#6200FF',
     alignItems: 'center',
     justifyContent: 'center',
   },
