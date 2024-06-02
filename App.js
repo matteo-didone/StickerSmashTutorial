5fb5e31e579ea79856f83b4e2b90c4fc00ba5dae
Matteo Didone <matteo.dido9@gmail.com>
2024-06-02 16:32:35 +0200

    Added the possibility to stick the sticker on top of the picture

--- App.js
+++ App.js
@@ -9,6 +9,7 @@ import CircleButton from './components/CircleButton';
 import IconButton from './components/IconButton';
 import EmojiPicker from "./components/EmojiPicker";
 import EmojiList from './components/EmojiList';
+import EmojiSticker from './components/EmojiSticker';
 
 const PlaceholderImage = require("./assets/images/background-image.png");
 
@@ -56,6 +57,9 @@ export default function App() {
           placeholderImageSource={PlaceholderImage}
           selectedImage={selectedImage}
         />
+        {pickedEmoji && (
+          <EmojiSticker imageSize={40} stickerSource={pickedEmoji} />
+        )}
       </View>
       {showAppOptions ? (
         <View style={styles.optionsContainer}>
