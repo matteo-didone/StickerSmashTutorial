1a9b72bab70e46674a300b4f7efffa0622b68f5b
Matteo Didone <matteo.dido9@gmail.com>
2024-06-04 18:17:50 +0200

    Added GestureDetector in EmojiSticker.js

--- components/EmojiSticker.js
+++ components/EmojiSticker.js
@@ -4,8 +4,15 @@ import Animated, { useAnimatedStyle, useSharedValue, withSpring } from 'react-na
 
 export default function EmojiSticker({ imageSize, stickerSource }) {
 
+    // The sticker moves along both axes, we need to track the X and Y values separately
     const translateX = useSharedValue(0);
     const translateY = useSharedValue(0);
+
+    const drag = Gesture.Pan()
+        .onChange((event) => {
+            translateX.value += event.changeX;
+            translateY.value += event.changeY;
+        });
 
     const scaleImage = useSharedValue(imageSize);
 
@@ -24,15 +31,30 @@ export default function EmojiSticker({ imageSize, stickerSource }) {
         };
     });
 
+    const containerStyle = useAnimatedStyle(() => {
+        return {
+            transform: [
+                {
+                    translateX: translateX.value,
+                },
+                {
+                    translateY: translateY.value,
+                },
+            ],
+        };
+    });
+
     return (
-        <Animated.View style={{ top: -350 }}>
-            <GestureDetector gesture={doubleTap}>
-                <Animated.Image
-                    source={stickerSource}
-                    resizeMode="contain"
-                    style={[imageStyle, { width: imageSize, height: imageSize }]}
-                />
-            </GestureDetector>
-        </Animated.View>
+        <GestureDetector gesture={drag}>
+            <Animated.View style={[containerStyle, { top: -350 }]}>
+                <GestureDetector gesture={doubleTap}>
+                    <Animated.Image
+                        source={stickerSource}
+                        resizeMode="contain"
+                        style={[imageStyle, { width: imageSize, height: imageSize }]}
+                    />
+                </GestureDetector>
+            </Animated.View>
+        </GestureDetector>
     );
 }
