2b4a83632fc807266b2983c52c3f00ecb6e10ffb
Matteo Didone <matteo.dido9@gmail.com>
2024-06-04 18:32:42 +0200

    Added Screenshot

--- App.js
+++ App.js
@@ -3,6 +3,9 @@ import { StyleSheet, View } from "react-native";
 import * as ImagePicker from 'expo-image-picker';
 import { useState } from 'react';
 import { GestureHandlerRootView } from "react-native-gesture-handler";
+import * as MediaLibrary from 'expo-media-library';
+import { captureRef } from 'react-native-view-shot';
+
 
 import Button from './components/Button';
 import ImageViewer from './components/ImageViewer';
@@ -19,6 +22,7 @@ export default function App() {
   const [selectedImage, setSelectedImage] = useState(null);
   const [isModalVisible, setIsModalVisible] = useState(false);
   const [pickedEmoji, setPickedEmoji] = useState(null);
+  const [status, requestPermission] = MediaLibrary.usePermissions();
 
   // Define functions here
   const onReset = () => {
@@ -34,7 +38,19 @@ export default function App() {
   };
 
   const onSaveImageAsync = async () => {
-    // Implement this later
+    try {
+      const localUri = await captureRef(imageRef, {
+        height: 440,
+        quality: 1,
+      });
+
+      await MediaLibrary.saveToLibraryAsync(localUri);
+      if (localUri) {
+        alert("Saved!");
+      }
+    } catch (e) {
+      console.log(e);
+    }
   };
 
   const pickImageAsync = async () => {
@@ -51,13 +67,21 @@ export default function App() {
     }
   };
 
+  const imageRef = useRef();
+
+  if (status === null) {
+    requestPermission();
+  }
+
   return (
     <GestureHandlerRootView style={styles.container}>
       <View style={styles.imageContainer}>
-        <ImageViewer
-          placeholderImageSource={PlaceholderImage}
-          selectedImage={selectedImage}
-        />
+        <View ref={imageRef} collapsable={false}>
+          <ImageViewer
+            placeholderImageSource={PlaceholderImage}
+            selectedImage={selectedImage}
+          />
+        </View>
         {pickedEmoji && (
           <EmojiSticker imageSize={40} stickerSource={pickedEmoji} />
         )}
